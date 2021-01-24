34baa321e67fd74d3ad94e69985aca7c0d804f70
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2021-01-25 01:08:42 +0600

    Testing successful

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePage
+from .views import HomePage, BlogDetailView
 
 urlpatterns = [
 
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('', HomePage.as_view(), name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Blog
 
 
@@ -10,3 +10,9 @@ class HomePage(ListView):
     model = Blog
     template_name = 'index.html'
     context_object_name = 'all_blog_posts'
+
+
+class BlogDetailView(DetailView):
+
+    model =Blog
+    template_name ='post_detail.html'
